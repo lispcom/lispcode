28cd731e3d686623935ee9e6dab4819402c7f5c8
wayne <278972894@qq.com>
2016-03-16 17:53:04 +0800

    , and @ use in common lisp

---
+++ de.lisp
@@ -0,0 +1,28 @@
+(setf a 1 b 2)
+(print `(a is ,a and b is ,b))
+
+
+(defmacro nil! (x)
+ `(setf ,x nil))
+
+
+(defmacro nill! (x)
+ (list 'setf x nil))
+
+(print (nil! a))
+(print (nill! b))
+
+
+(setf lst '(a b c))
+(print lst)
+
+(setf lstt `(a b c))
+(print lstt)
+
+(print `(lst is ,lst))
+(print `(lst is ,@lst))
+
+
+
+
+
