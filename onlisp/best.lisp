a3eff8e18a289fd203b8c7870bff99465ccf49f4
decora <278972894@qq.com>
2016-09-03 21:56:40 +0800

    best

---
+++ onlisp/best.lisp
@@ -0,0 +1,12 @@
+(defun best (fn lst)
+  (if (null lst)
+    nil
+    (let ((wins (car lst)))
+      (dolist (obj (cdr lst))
+        (if (funcall fn obj wins)
+          (setq wins obj)))
+      wins)))
+
+(print
+  (best #'> '(1 2 3 4 5))
+  )
